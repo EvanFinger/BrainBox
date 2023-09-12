a53dc8487330237ac9e9448e178e8f823be03614
Evan Finger <evanfinger@outlook.com>
2023-09-12 02:52:41 -0400

    Started Work on UI creation in terminal.

--- BrainBox_Proj/BrainBox_Proj/ConStates.cpp
+++ BrainBox_Proj/BrainBox_Proj/ConStates.cpp
@@ -4,12 +4,14 @@
 
 MainMenu::MainMenu()
 {
-	this->con_text.add_layer
-	(
-		std::vector<std::string>
-		{
-			"Welcome to the Grade Manager!",
-			"  Select an existing file, or create a new one..."
-		}
-	);
+	this->con_text = new box_struct(17, 5);
+	this->con_text->box_text.add_text("Hello World");
+	this->con_text->format_box(TOP, CENTER);
+	
 }
+
+MainMenu::~MainMenu()
+{
+	delete this->con_text;
+}
+
--- BrainBox_Proj/BrainBox_Proj/ConStates.h
+++ BrainBox_Proj/BrainBox_Proj/ConStates.h
@@ -7,10 +7,19 @@
 #include <tuple>
 #include <map> 
 
-struct string_struct
+enum alignment
 {
-	std::vector<std::string> text;
-	std::map<std::string, std::string> color = 
+	LEFT = 0,
+	CENTER,
+	RIGHT,
+	TOP,
+	BOTTOM
+};
+
+struct printable_struct
+{
+	std::vector<std::string> printable;
+	std::map<std::string, std::string> color =
 	{
 		{"RESET", "\033[0m"},
 		{"BLACK", "\033[30m"},
@@ -21,36 +30,150 @@ struct string_struct
 		{"PURPLE", "\033[35m"},
 		{"CYAN", "\033[36m"},
 	};
+
+	printable_struct() {};
+
+	void add_text(std::vector<std::string> _txt_obj)
+	{
+		for (auto& layer : _txt_obj)
+		{
+			this->printable.push_back(layer);
+		}
+	}
+
+	void add_text(std::string _txt)
+	{
+		this->printable.push_back(_txt);
+	}
+
+	void print()
+	{
+		for (auto& _string : this->printable)
+		{
+				std::cout << _string << "\n";
+		}
+	}
+};
+
+struct string_struct
+	:printable_struct
+{
 	
 	string_struct()
 	{
 	}
 
-	void add_layer(std::string layer_text)
-	{
-		this->text.push_back(layer_text);
-	}
+};
 
-	void add_layer(std::vector<std::string> layers)
+struct box_struct
+	:printable_struct
+{
+	
+	string_struct box_text;
+	unsigned short width, height;
+
+	box_struct(unsigned short width, unsigned short height)
 	{
-		for (auto& str : layers)
+		this->width = width;
+		this->height = height;
+
+		for (int i = 0; i < this->height; i++)
 		{
-			this->text.push_back(str);
+
+			this->printable.push_back("");
+			for (int j = 0; j < this->width; j++)
+			{
+				if (i == 0 || i == this->height - 1)
+				{
+					this->printable.back().append("-");
+				}
+				else if (j == 0 || j == this->width - 1)
+				{
+					this->printable.back().append("|");
+				}
+				else
+				{
+					this->printable.back().append(" ");
+				}
+			}
+		}
+	}
+	
+	void format_row(alignment horizontal, unsigned short row_num)
+	{
+		unsigned short row_num_txt = row_num - 1;
+		unsigned short char_num = 0;
+		unsigned short text_size = NULL;
+		if (this->box_text.printable.size() > row_num_txt )
+		{
+			 text_size = this->box_text.printable[row_num_txt].size();
+		}
+		switch (horizontal)
+		{
+		case LEFT:
+			for (size_t i = 0; i < this->width; i++)
+			{
+				if (i == 0 || i == this->width - 1)
+				{
+					this->printable[row_num].append("|");
+				}
+				else if (i <= text_size)
+				{
+					
+					this->printable[row_num].push_back(this->box_text.printable[row_num_txt][char_num]);
+					char_num++;
+				}
+				else
+				{
+					this->printable[row_num].append(" ");
+				}
+			}
+			break;
+		case CENTER:
+			for (size_t i = 0; i < this->width; i++)
+			{
+				
+				if (i == 0 || i == this->width - 1)
+				{
+					this->printable[row_num].append("|");
+				}
+				else if (i >= ((this->width - text_size) / 2)  && i < ((this->width - text_size) / 2)  + text_size)
+				{
+					this->printable[row_num].push_back(this->box_text.printable[row_num_txt][char_num]);
+					char_num++;
+				}
+				else
+				{
+					this->printable[row_num].append(" ");
+				}
+			}
+		}
+	}
+	void format_box(alignment vertical, alignment horizontal)
+	{
+		//Check Boundings
+		for (auto& string : this->box_text.printable)
+		{
+			if (string.size() > this->width - 2)
+			{
+				throw "EXEMPTION IN BOX STRUCT: STRING LEAVES BOUNDS";
+			}
+		}
+		if (this->box_text.printable.size() > this->height - 2)
+		{
+			throw "EXEMPTION IN BOX STRUCT: TEXT PRINTABLE LEAVES BOUNDS";
+		}
+
+		if (vertical == TOP)
+		{
+			for (int i = 1; i < this->printable.size() - 1; i++)
+			{
+				this->printable[i] = "";
+				this->format_row(horizontal, i);
+			}
 		}
 	}
 
-	void delete_layer(int layer)
-	{
-		this->text[layer].erase();
-	}
-
-	void print()
-	{
-		for (auto& layer : text)
-		{
-			std::cout << layer << "\n";
-		}
-	}
 };
 
 class ConState
@@ -62,11 +185,10 @@ public:
 	}
 	~ConState()
 	{
-
 	}
 	
 protected:
-	string_struct con_text;
+	box_struct* con_text;
 
 private:
 	
@@ -78,9 +200,10 @@ class MainMenu
 {
 public:
 	MainMenu();
+	~MainMenu();
 	void print()
 	{
-		this->con_text.print();
+		this->con_text->print();
 	}
 protected:
 
